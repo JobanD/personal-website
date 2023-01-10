3eb5c689c7fb31d5a746c92a0d18fdbfa6e49afa
JobanD <joban.d555@gmail.com>
2023-01-10 15:47:08 -0500

    Changed svg wave animations to work on safari as well

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,6 @@ import ProfilePhoto from "../assets/headshot.JPEG";
 
 export default function Home() {
   const [height, width] = useWindowSize(); // height and width of viewport
-  const projectsWaveStyle = { transform: "rotate(180) translate(700 0)" };
-  const projectsWaveStyle2 = {
-    transform: "rotate(180) tscale(-1 1) translate(-100 0)",
-  };
 
   // CURSOR
 
@@ -120,7 +116,7 @@ export default function Home() {
               ></path>
             </svg>
           </div>
-          <div className="wave-about2 wave">
+          {/* <div className="wave-about2 wave">
             <svg
               data-name="Layer 1"
               xmlns="http://www.w3.org/2000/svg"
@@ -133,21 +129,23 @@ export default function Home() {
                 className="shape-fill"
               ></path>
             </svg>
-          </div>
+          </div> */}
         </div>
       </section>
       <section className="reveal fade-right">
         <Skills />
       </section>
       <section className="reveal fade-bottom">
-        <div className="project-waves">
-          <div className="wave-projects wave">
+        <div
+          className="project-waves"
+          style={{ transform: "rotate(180deg) translateX(-100%)" }}
+        >
+          {/* <div className="wave-projects wave">
             <svg
               data-name="Layer 1"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1650 120"
               preserveAspectRatio="none"
-              style={projectsWaveStyle}
               // transform="rotate(180) translate(700 0)"
             >
               <path
@@ -155,14 +153,14 @@ export default function Home() {
                 class="shape-fill"
               ></path>
             </svg>
-          </div>
+          </div> */}
           <div className="wave-projects2 wave">
             <svg
               data-name="Layer 1"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1650 120"
               preserveAspectRatio="none"
-              style={projectsWaveStyle2}
+              // transform="scale(-1 1) translate(-100 0)"
             >
               <path
                 d="M 321.4 56.4 c 58 -10.8 114.2 -30.1 172 -41.9 c 82.4 -16.7 168.2 -17.7 250.4 -0.4 C 823.8 31 906.7 72 985.7 92.8 c 70 18.5 146.5 26.1 268.3 -31.8 C 1290 44 1317 23 1340 0 V 0 H 0 V 3 A 600.2 600.2 0 0 0 321.4 56.4 Z"
